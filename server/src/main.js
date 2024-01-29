fe3767605b865ff66da94152cd60cd8145a64a81
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-01-29 13:38:32 +0545

    Update server configuration

--- server/src/main.js
+++ server/src/main.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 var express = require("express");
 var app = express();
-var PORT = 3000;
+var PORT = parseInt(process.env.PORT || '3333');
 app.get('/', function (req, res) {
     res.send('Hello, TypeScript with Express!');
 });
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+
+const PORT : number = parseInt(process.env.PORT || '3333');
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
@@ -10,3 +12,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
