f16957253fc7c7b5e5dd767214747f87d450c754
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-02-05 13:10:57 +0545

    Add rate limiting middleware to API

--- nosql/index.ts
+++ nosql/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
+import { rateLimit } from 'express-rate-limit'
 import compression from "compression";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -10,20 +11,30 @@ import ErrorHandler from "./middlewares/ErrorHandler";
 
 const PORT = parseInt(process.env.PORT || "3333");
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message:"Too many requests, please try again later."
+})
+
+
 if (!process.env.DB_URL) {
   throw new Error("DB_URL environment variable is not defined");
 }
 const DB_URL: string = process.env.DB_URL;
 mongoose
-  .connect(DB_URL)
-  .then(() => {
-    console.log("Database is connected");
-  })
-  .catch((error) => {
-    console.error("Database connection error:", error);
-  });
+.connect(DB_URL)
+.then(() => {
+  console.log("Database is connected");
+})
+.catch((error) => {
+  console.error("Database connection error:", error);
+});
 
 const app = express();
+app.use(limiter)
 
 app.use(express.json());
 app.use(cors());
