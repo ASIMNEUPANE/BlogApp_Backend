5a86283b7298f4fb8cb540fa1d2c389ef25b50b5
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-02-06 11:56:22 +0545

    Add rate limiting middleware

--- nosql/index.ts
+++ nosql/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
-import { rateLimit } from 'express-rate-limit'
+import limiter from "./middlewares/rateLimit";
 import compression from "compression";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -11,31 +11,20 @@ import ErrorHandler from "./middlewares/ErrorHandler";
 
 const PORT = parseInt(process.env.PORT || "3333");
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  message:"Too many requests, please try again later."
-})
-
-
 if (!process.env.DB_URL) {
   throw new Error("DB_URL environment variable is not defined");
 }
 const DB_URL: string = process.env.DB_URL;
 mongoose
-.connect(DB_URL)
-.then(() => {
-  console.log("Database is connected");
-})
-.catch((error) => {
-  console.error("Database connection error:", error);
-});
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
 
 const app = express();
-app.use(limiter)
-
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
@@ -50,9 +39,9 @@ app.use(
     },
   })
 );
+app.use(limiter);
 app.use("/", IndexRouter);
-
-app.use(ErrorHandler)
+app.use(ErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
--- nosql/middlewares/ErrorHandler.ts
+++ nosql/middlewares/ErrorHandler.ts
@@ -7,12 +7,12 @@ const ErrorHandler = (
   next: NextFunction
 ) => {
   console.log("Middleware Error Handling");
-
   const errStatus = err.statusCode || 500;
 
-  let errType = err.errors?.length ? "ZOD" : "MONGO";
+  const errType = err.errors?.length ? "ZOD" : "ratelimit";
 
-  let errMsg = errType === "ZOD" ? err.errors[0].message : err.toJSON().message
+  // let errMsg = errType === "ZOD" ? err.errors[0].message : err.toJSON().message
+  const errMsg = errType === "ZOD" ? err.errors[0].message : err.message;
 
   res.status(errStatus).json({
     success: false,
@@ -20,8 +20,6 @@ const ErrorHandler = (
     message: errMsg,
     stack: process.env.NODE_ENV === "development" ? err.stack : {},
   });
-
-  
 };
 
 export default ErrorHandler;
