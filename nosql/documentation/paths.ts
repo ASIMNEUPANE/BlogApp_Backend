8f452b8af3054b7c859a9a822211b4dd8132d6b1
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-02-11 18:37:49 +0545

    cleanup

--- nosql/documentation/paths.ts
+++ nosql/documentation/paths.ts
@@ -1,206 +1,220 @@
 export default {
-    paths: {
-      // Blog API
-  
-      "/blogs": {
-        post: {
-          tags: ["blogs"],
-          summary: "Create a new blog",
-          description: "Return newly added blog",
-          operationId: "add",
-          consumes: ["application/json"],
-          produces: ["application/json"],
-          parameters: [
-            {
-              in: "body",
-              name: "body",
-              description: "Required payload for creating new blog",
-              required: true,
-              schema: {
-                $ref: "#/definitions/blogs"
-              }
-            }
-          ],
-          // security: [
-          //   {
-          //     access_token: []
-          //   }
-          // ],
-          responses: {
-            200: {
-              description: "Successful operation",
-              schema: {
-                $ref: "#/definitions/blogs"
-              }
+  paths: {
+    // Blog API
+
+    "/blogs": {
+      post: {
+        tags: ["blogs"],
+        summary: "Create a new blog",
+        description: "Return newly added blog",
+        operationId: "add",
+        consumes: ["application/json"],
+        produces: ["application/json"],
+        parameters: [
+          {
+            in: "body",
+            name: "body",
+            description: "Required payload for creating new blog",
+            required: true,
+            schema: {
+              $ref: "#/definitions/blogs",
             },
-            500: {
-              description: "Invalid blogs"
-            }
-          }
+          },
+        ],
+        // security: [
+        //   {
+        //     access_token: []
+        //   }
+        // ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/blogs",
+            },
+          },
+          500: {
+            description: "Invalid blogs",
+          },
         },
-  
-        // Get
-  
-        get: {
-          tags: ["blogs"],
-          summary: "Get all the blogs",
-          description: "Returns all the blogs",
-          operationId: "list",
-          consumes: ["application/json"],
-          produces: ["application/json"],
-          parameters: [],
-          // security: [
-          //   {
-          //     access_token: []
-          //   }
-          // ],
-          responses: {
-            200: {
-              description: "Successful operation",
-              schema: {
-                $ref: "#/definitions/blogs"
-              }
-            },
-            500: {
-              description: "Invalid Operation"
-            }
-          }
-        }
       },
-  
-      // GetById
-  
-      "/blogs/{id}": {
-        get: {
-          tags: ["blogs"],
-          summary: "Find blogs by ID",
-          description: "Returns a single blog",
-          operationId: "getById",
-          produces: ["application/json"],
-          parameters: [
-            {
-              name: "id",
-              in: "path",
-              description: "ID of blog to return",
-              required: true,
-              type: "string"
-            }
-          ],
-          // security: [
-          //   {
-          //     access_token: []
-          //   }
-          // ],
-          responses: {
-            200: {
-              description: "Successful operation",
-              schema: {
-                $ref: "#/definitions/blogs"
-              }
+
+      // Get
+
+      get: {
+        tags: ["blogs"],
+        summary: "Get all the blogs",
+        description: "Returns all the blogs",
+        operationId: "list",
+        // consumes: ["application/json"],
+        produces: ["application/json"],
+        parameters: [
+          {
+            name: "limit",
+            in: "body",
+            description: "limit of blog to return in a page",
+            required: true,
+            type: "number",
+          },
+          {
+            name: "page",
+            in: "body",
+            description: "page no. of blog to return",
+            required: true,
+            type: "number",
+          },
+        ],
+        // security: [
+        //   {
+        //     access_token: []
+        //   }
+        // ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/blogs",
             },
-            400: {
-              description: "Invalid ID supplied"
-            },
-            500: {
-              description: "Invalid Operation"
-            },
-            404: {
-              description: "blog not found"
-            }
-          }
+          },
+          500: {
+            description: "Invalid Operation",
+          },
         },
-  
-        // Update
-  
-        put: {
-          tags: ["blogs"],
-          summary: "Update blog status by ID",
-          description: "Returns a single blog with updated status",
-          operationId: "update",
-          produces: ["application/json"],
-          parameters: [
-            {
-              name: "id",
-              in: "path",
-              description: "ID of blog to return",
-              required: true,
-              type: "string"
+      },
+    },
+
+    // GetById
+
+    "/blogs/{id}": {
+      get: {
+        tags: ["blogs"],
+        summary: "Find blogs by ID",
+        description: "Returns a single blog",
+        operationId: "getById",
+        produces: ["application/json"],
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID of blog to return",
+            required: true,
+            type: "string",
+          },
+        ],
+        // security: [
+        //   {
+        //     access_token: []
+        //   }
+        // ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/blogs",
             },
-            {
-              in: "body",
-              name: "body",
-              description: "Required payload for creating new blog",
-              required: true,
-              schema: {
-                $ref: "#/definitions/blogs"
-              }
-            }
-          ],
-          // security: [
-          //   {
-          //     access_token: []
-          //   }
-          // ],
-          responses: {
-            200: {
-              description: "Successful operation",
-              schema: {
-                $ref: "#/definitions/blogs"
-              }
-            },
-            400: {
-              description: "Invalid ID supplied"
-            },
-            500: {
-              description: "Invalid Operation"
-            },
-            404: {
-              description: "Todo not found"
-            }
-          }
+          },
+          400: {
+            description: "Invalid ID supplied",
+          },
+          500: {
+            description: "Invalid Operation",
+          },
+          404: {
+            description: "blog not found",
+          },
         },
-  
-        // Delete
-  
-        delete: {
-          tags: ["blogs"],
-          summary: "Delete todo status by ID",
-          description: "Deletes a single todo",
-          operationId: "delete",
-          produces: ["application/json"],
-          parameters: [
-            {
-              name: "id",
-              in: "path",
-              description: "ID of todo that needs to be deleted",
-              required: true,
-              type: "string"
-            }
-          ],
-          // security: [
-          //   {
-          //     access_token: []
-          //   }
-          // ],
-          responses: {
-            200: {
-              description: "Successful operation",
-              schema: {
-                $ref: "#/definitions/blogs"
-              }
+      },
+
+      // Update
+
+      put: {
+        tags: ["blogs"],
+        summary: "Update blog status by ID",
+        description: "Returns a single blog with updated status",
+        operationId: "update",
+        produces: ["application/json"],
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID of blog to return",
+            required: true,
+            type: "string",
+          },
+          {
+            in: "body",
+            name: "body",
+            description: "Required payload for creating new blog",
+            required: true,
+            schema: {
+              $ref: "#/definitions/blogs",
             },
-            400: {
-              description: "Invalid ID supplied"
+          },
+        ],
+        // security: [
+        //   {
+        //     access_token: []
+        //   }
+        // ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/blogs",
             },
-            500: {
-              description: "Invalid Operation"
+          },
+          400: {
+            description: "Invalid ID supplied",
+          },
+          500: {
+            description: "Invalid Operation",
+          },
+          404: {
+            description: "Todo not found",
+          },
+        },
+      },
+
+      // Delete
+
+      delete: {
+        tags: ["blogs"],
+        summary: "Delete todo status by ID",
+        description: "Deletes a single todo",
+        operationId: "delete",
+        produces: ["application/json"],
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID of todo that needs to be deleted",
+            required: true,
+            type: "string",
+          },
+        ],
+        // security: [
+        //   {
+        //     access_token: []
+        //   }
+        // ],
+        responses: {
+          200: {
+            description: "Successful operation",
+            schema: {
+              $ref: "#/definitions/blogs",
             },
-            404: {
-              description: "Todo not found"
-            }
-          }
-        }
-      }
-    }
-  };
-  
+          },
+          400: {
+            description: "Invalid ID supplied",
+          },
+          500: {
+            description: "Invalid Operation",
+          },
+          404: {
+            description: "Todo not found",
+          },
+        },
+      },
+    },
+  },
+};
