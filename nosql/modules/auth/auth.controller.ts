9b48785346bbfd74635fe0a0ce27c553d8daaa09
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-02-14 10:20:31 +0545

    Add auth controller for registration

---
+++ nosql/modules/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import bcrypt from "bcrypt";
+import model from './auth.model'
+import userModel from "../users/user.model";
+
+import {generateOTP} from "../../utils/otp"
+import { authData } from './auth.types'
+import {mailer} from '../../services/mailer';
+
+const register = async (payload: authData): Promise<unknown> => {
+  try {
+    const { roles, password, ...rest } = payload;
+    rest.password = await bcrypt.hash(password, Number(process.env.SAL_ROUND));
+    const user = await userModel.create(rest);
+    const token = await generateOTP();
+    console.log(typeof(token));
+    await model.create({ email: user?.email, token });
+    const mail = await mailer(user?.email, token);
+    return mail;
+  } catch (error) {
+    console.error("Error during registration:", error);
+    throw error; // Rethrow the error for handling at a higher level if needed
+  }
+};
+
+export default {
+  register
+};
