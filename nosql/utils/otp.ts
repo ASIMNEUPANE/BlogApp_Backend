62da9edeb7f161c5729001b50190a8236fb0a21e
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-02-14 10:20:07 +0545

    Add OTP utility functions

---
+++ nosql/utils/otp.ts
@@ -0,0 +1,19 @@
+import {totp} from "otplib";
+
+totp.options = { digits: 6, step: Number(process.env.OTP_DURATION) };
+require("dotenv").config();
+
+export const generateOTP = () => {
+  totp.options = { digits: 6, step: 120 };
+  return totp.generate(process.env.OTP_SECRET);
+};
+
+const verifyOTP = async (token) => {
+  totp.options = { digits: 6, step: 120 };
+  const x = totp.check(token, process.env.OTP_SECRET);
+  return x;
+};
+
+export default { generateOTP, verifyOTP };
+
+
