ea56e9fa5574675869818642eba7785fb88994a4
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-02-14 15:47:55 +0545

    Refactor auth module: Fix imports, add verify function

--- nosql/modules/auth/auth.controller.ts
+++ nosql/modules/auth/auth.controller.ts
@@ -1,17 +1,18 @@
 import bcrypt from "bcrypt";
-import model from './auth.model'
+import model from "./auth.model";
 import userModel from "../users/user.model";
 
-import {generateOTP} from "../../utils/otp"
-import { authData } from './auth.types'
-import {mailer} from '../../services/mailer';
+import { generateOTP, verifyOTP } from "../../utils/otp";
+import { authData, verifyData } from "./auth.types";
+import { mailer } from "../../services/mailer";
 
 const register = async (payload: authData): Promise<unknown> => {
   try {
-    const { isActive, isEmailVerified,roles, password, ...rest } = payload;
+    const { isActive, isEmailVerified, roles, password, ...rest } = payload;
     rest.password = await bcrypt.hash(password, Number(process.env.SAL_ROUND));
     const user = await userModel.create(rest);
-    const token =  generateOTP();
+    const token = generateOTP();
+    console.log(typeof token);
     await model.create({ email: user?.email, token });
     const mail = await mailer(user?.email, token);
     return mail;
@@ -20,7 +21,27 @@ const register = async (payload: authData): Promise<unknown> => {
     throw error; // Rethrow the error for handling at a higher level if needed
   }
 };
+const verify = async (payload: verifyData): Promise<unknown> => {
+  const {email,token} = payload
+  const auth = await model.findOne({email});
+  if (!auth) throw new Error("User is not available");
+
+  const isValidToken = await verifyOTP(token);
+  if (!isValidToken) throw new Error("Token Expired");
+
+  const emailValid = auth?.token === +token;
+  if (!emailValid) throw new Error("Token mismatch");
+
+  const user = await userModel.findOneAndUpdate(
+    { email},
+    { isEmailVerified: true, isActive: true },
+    { new: true }
+  ).select("-password");
+  await model.deleteOne({ email });
+  return user
+};
 
 export default {
-  register
+  register,
+  verify,
 };
--- nosql/modules/auth/auth.routes.ts
+++ nosql/modules/auth/auth.routes.ts
@@ -1,12 +1,21 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import controllor from "./auth.controller"
-import {authValidatorMiddleware} from './auth.validator'
+import {authValidatorMiddleware,verifyAuthMiddleware} from './auth.validator'
 const router: Router = express.Router();
 
 router.post("/register",authValidatorMiddleware, async(req:Request,res:Response,next:NextFunction)=>{
     try{
       console.log(req.body)
         const result = await controllor.register(req.body)
+        res.status(200).json({data:result, msg:"success"})
+    }catch(e){
+      next(e)
+    }
+})
+router.post("/verify",verifyAuthMiddleware, async(req:Request,res:Response,next:NextFunction)=>{
+    try{
+      console.log(req.body)
+        const result = await controllor.verify(req.body)
         res.status(200).json({data:result, msg:"success"})
     }catch(e){
       next(e)
