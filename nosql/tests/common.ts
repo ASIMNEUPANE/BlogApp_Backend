cdbe43d931f501ebc58cd0483a689034a8b69f9e
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-02-21 14:23:16 +0545

    cleanup

--- nosql/tests/common.ts
+++ nosql/tests/common.ts
@@ -1,32 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-// Define interface outside the object
 interface DatabaseModule {
   connectDatabase: () => Promise<void>;
   closeDatabase: (done: any) => Promise<void>;
 }
-
 const common: DatabaseModule = {
-  async connectDatabase() {
-    try {
-      await mongoose.connect((global as any).__MONGO_URI__, {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
-      });
-      await mongoose.connection.db.dropDatabase();
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-    }
+   async connectDatabase() {
+    await mongoose.connect((global as any).__MONGO_URI__, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+    await mongoose.connection.db.dropDatabase();
   },
 
   async closeDatabase(done:any) {
-    try {
-      await mongoose.connection.db.dropDatabase();
-    } catch (error) {
-      console.error("Error dropping MongoDB database:", error);
-    } finally {
-      await mongoose.disconnect(done);
-    }
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.disconnect(done);
   },
 };
 
--- nosql/tests/controllers/blogs.controller.test.ts
+++ nosql/tests/controllers/blogs.controller.test.ts
@@ -1,6 +1,6 @@
 import common from "../common";
-import { create, get } from "../../modules/blog/blog.controller";
-import MongoClient from "mongodb";
+import { create, get, getById } from "../../modules/blog/blog.controller";
+// import MongoClient from "mongodb";
 
 const blogData = {
   title: "wonder-world",
@@ -21,20 +21,15 @@ const getData = {
 // Add Operation
 
 describe("Blog Model Test", () => {
-  let connection;
-  let db;
-
   beforeAll(async () => {
-    connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db = await connection.db(globalThis.__MONGO_DB_NAME__);
+    await common.connectDatabase();
+
+  });
+  afterAll(async () => {
+    await common.closeDatabase();
   });
 
-  afterAll(async () => {
-    await connection.close();
-  });
+  // Create Operation
 
   it("create & save blog successfully", async () => {
     console.log("Creating blog...");
@@ -54,13 +49,30 @@ describe("Blog Model Test", () => {
   });
   // List Operation
 
-  it("lists all todos with their subtasks", async () => {
+  it("lists all blogs", async () => {
     const allData = await get(2, 2, "");
 
-    expect(allData).toBeDefined();
-    expect(Array.isArray(allData)).toBe(true);
-  });
+   // Assertion to check if allData is an object
+  expect(typeof allData).toBe('object');
 
-  // List Operation
-  // Add your list operation test cases here
+  // Assertion to check if allData contains a property named 'data'
+  expect(allData).toHaveProperty('data');
+
+  // Assertion to check if 'data' property is an array
+  expect(Array.isArray(allData.data)).toBe(true);
+
+  // Assertion to check if each item in 'data' array is an object
+  allData.data.forEach(item => {
+    expect(typeof item).toBe('object');
+  });
 });
+
+  // Get By Id Operation
+  it("Get a blog by id",async()=>{
+    const createdBlog = await create(blogData);
+    const getData = await getById(createdBlog._id)
+    expect(getData).toBeDefined();
+    expect(getData.title).toBe(getData.title)
+  })
+} 
+)
