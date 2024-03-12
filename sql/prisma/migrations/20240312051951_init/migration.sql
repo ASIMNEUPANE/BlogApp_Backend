ae7802517b73a6edd62dd7acb4c45e44428451eb
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-12 11:05:03 +0545

    Update Category enum in migration.sql

---
+++ sql/prisma/migrations/20240312051951_init/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [TRAVEL,FOOD,LIFESTYLE] on the enum `Category` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Category_new" AS ENUM ('TECHNOLOGY', 'Travel', 'Food', 'Lifestyle');
+ALTER TABLE "Blog" ALTER COLUMN "category" DROP DEFAULT;
+ALTER TABLE "Blog" ALTER COLUMN "category" TYPE "Category_new" USING ("category"::text::"Category_new");
+ALTER TYPE "Category" RENAME TO "Category_old";
+ALTER TYPE "Category_new" RENAME TO "Category";
+DROP TYPE "Category_old";
+ALTER TABLE "Blog" ALTER COLUMN "category" SET DEFAULT 'TECHNOLOGY';
+COMMIT;
